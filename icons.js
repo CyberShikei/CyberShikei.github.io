084386fb724d01c8590bea7657a743feff11aefa
CyberShikei <jacopferreira@gmail.com>
2025-02-19 00:31:15 +0200

    updateMenu

--- icons.js
+++ icons.js
@@ -1,11 +1,10 @@
 const images = "/images";
 
 const icons = [
-    { name: "profile_pic", url: "https://github.com/CyberShikei", img: `${images}/profile_pic.jpg` },
-    { name: "GitHub", url: "https://github.com/", img: `${images}/github_logo.png` },
-    { name: "LinkedIn", url: "https://www.linkedin.com/", img: `${images}/linkedin_logo.png` },
-    { name: "Google", url: "https://www.google.com/", img: `${images}/google_logo.png` },
-    { name: "YouTube", url: "https://www.youtube.com/", img: `${images}/youtube_logo.png` }
+    { name: "profile_pic", url: "CyberShikei.github.io", img: `${images}/profile_pic.jpg` },
+    { name: "GitHub", url: "https://github.com/CyberShikei", img: `${images}/github_logo.png` },
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/jacobus-petrus-ferreira-b888141a1/", img: `${images}/linkedin_logo.png` },
+    { name: "Gmail", url: "mailto:cyber.shikei.com", img: `${images}/gmail_logo.png` },
 ];
 
 const iconContainer = document.getElementById("icon-container");
