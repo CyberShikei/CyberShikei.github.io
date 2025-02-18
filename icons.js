4aaa4b0b8201609351cc4be62c1409a2b4d313be
CyberShikei <jacopferreira@gmail.com>
2025-02-18 23:14:53 +0200

    {update}

--- icons.js
+++ icons.js
@@ -1,6 +1,7 @@
 const images = "/images";
 
 const icons = [
+    { name: "profile_pic", url: "https://github.com/CyberShikei", img: `${images}/profile_pic.jpg` },
     { name: "GitHub", url: "https://github.com/", img: `${images}/github_logo.png` },
     { name: "LinkedIn", url: "https://www.linkedin.com/", img: `${images}/linkedin_logo.png` },
     { name: "Google", url: "https://www.google.com/", img: `${images}/google_logo.png` },
@@ -14,8 +15,8 @@ icons.forEach(icon => {
     iconElement.classList.add("icon");
 
     iconElement.innerHTML = `
-        <a href="${icon.url}" target="_blank">
-            <img src="${icon.img}" alt="${icon.name}">
+        <a href="${icon.url}" target="_blank" class="icon-link", id="${icon.name}">
+            <img src="${icon.img}" alt="${icon.name}" class="icon-image" id="${icon.name}">
         </a>
     `;
 
